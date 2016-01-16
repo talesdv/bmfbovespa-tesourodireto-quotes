a2ced37d425925aaadf6dafc2e0b102a1178f9d1
Tales <talesdv@gmail.com>
2016-01-16 17:29:03 -0200

    Read TD prices

--- index.js
+++ index.js
@@ -2,22 +2,79 @@ const yahooFinance = require('yahoo-finance');
 const async	       = require('async');
 const fs 		   = require('fs');
 const csvStringify = require('csv-stringify');
+const yql 		   = require('yql');
+
+var symbols_json   = require('./symbols.json');
 
 async.parallel([
 	function(callback) {
 		yahooFinance.snapshot({
-		  symbols: ['ABEV3.SA', 'BBAS3.SA'],
-		  fields: ['s', 'd1', 'l1']  // ex: ['s', 'n', 'd1', 'l1', 'y', 'r']
+		  	symbols: symbols_json,
+
+		  	fields: ['s', 'd1', 'l1']  // ex: ['s', 'n', 'd1', 'l1', 'y', 'r']
 		}, function (err, snapshot) {
-		  if(err)
-		  	callback(err);
-		  else
-		  	callback(null, snapshot);
+		  	if(err)
+		  		callback(err);
+		  	else
+		  		callback(null, snapshot);
 		});	
 	},
 
 	function updateFromTD(callback) {
-		callback(null, 0);
+		var query = 'select * from html where (url = @url) and (xpath = @xpath)';
+		var base_url = 'http://www.tesouro.fazenda.gov.br/tesouro-direto-precos-e-taxas-dos-titulos';
+
+		// XPATH should be: //*[@id="p_p_id_precosetaxas_WAR_tesourodiretoportlet_"]/div/div/div/table[2]/tbody
+		var xpath = '//*[@id=\"p_p_id_precosetaxas_WAR_tesourodiretoportlet_\"]/div/div/div/table[2]/tbody'
+
+		var query_yql = new yql(query)
+			.setParam('url', base_url)
+			.setParam('xpath', xpath);
+
+		query_yql.exec(function(err, response) {
+			// console.log(response.query.results.tbody.tr);
+			var table = response.query.results.tbody.tr;
+			var response_filtered = [];
+
+			const filterElementClass  = 'camposTesouroDireto';
+			const indexPricePrimary	  = 4;
+			const indexPriceSecondary = 5;
+			const indexName			  = 0;
+
+			var td_line;
+			var price_string;
+			var price_float;
+
+			for(var i=0; i<table.length; i++) {
+				if(table[i].class == filterElementClass) {
+					td_line = table[i];
+
+					if(td_line.td[indexPricePrimary].content != 'R$0,00')
+						price_string = td_line.td[indexPricePrimary].content;
+					else
+						price_string = td_line.td[indexPriceSecondary].content;
+
+					// Remove 'R$' from price (first two characters)
+					price_string = price_string.replace('R$', '');
+
+					// Remove point if it exists
+					price_string = price_string.replace('.', '');
+
+					// Replace comma with point
+					price_string = price_string.replace(',', '.');
+
+					price_float = parseFloat(price_string);
+
+					response_filtered.push({
+						'symbol'		: td_line.td[indexName].content,
+						'lastTradeDate'	: '',
+						'lastTradePrice': price_float
+					});
+				}
+			}
+
+			callback(null, response_filtered);
+		});
 	}
 
 	],
@@ -36,7 +93,7 @@ async.parallel([
 							callback(err);
 						else
 							callback(null, csv);
-					}
+					});
 				},
 
 				function(csv, callback) {
@@ -59,6 +116,8 @@ async.parallel([
 	}
 );
 
+
+
 function refactorCSV() {
 	// Convert timestamp
 	// Remove .SA
@@ -75,13 +134,10 @@ function createCSV(data, callback) {
 };
 
 function writeToFile(data, path, file_name, callback) {
-	fs.writeFile(path + file_name, data, (err) => {
-		if (err) {
+	fs.writeFile(path + file_name, data, function(err) {
+		if (err)
 	  		callback(err);
-	  		return;
-	  	}
-		else {
-			console.log('It\'s saved!');
-		}
+		else
+			callback(null);
 	});
 };
