914616a86dbed2559b59b2e0d5de7271dc78e12d
i850706 <tales.vecchia@sap.com>
2020-07-04 12:31:01 -0300

    use https and new TD API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const yahooFinance = require('yahoo-finance');
 const async	       = require('async');
 const fs 		   = require('fs');
 const csvStringify = require('csv-stringify');
-const http		   = require('http');
+const http		   = require('https');
 const cheerio 	   = require('cheerio');
 
 const td_list	   = require('./td_list.json');
@@ -10,7 +10,6 @@ const stock_list   = require('./stock_list.json');
 
 async.parallel([
 	function updateYahooFinance(callback) {
-		var stock_symbols = [];
 		var quotes_array = [];
 
 		async.forEachOf(stock_list, function(result, i, for_callback) {
@@ -26,72 +25,41 @@ async.parallel([
 					quotes_array.push({
 						"name" 				 : mapSymbolIn2Out(quote.price.symbol, stock_list),
 						"lastTradePriceOnly" : quote.price.regularMarketPrice, 
-						// "lastTradeDate"		 : 
 					});
 
 					for_callback(null);
 			});	
 		}, function(err) {
-			if(err)
+			if(err) {
 				console.log(err);
+			}
 
-			console.log(quotes_array);
 			callback(err, quotes_array);
 		});	
 	},
 
 	function updateFromTD(callback) {
+		var api_url = 'https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json';
+		let body = '';
 
-		//var base_url = 'http://www3.tesouro.gov.br/tesouro_direto/consulta_titulos_novosite/consultatitulos.asp';
-		var base_url = 'http://www.tesouro.fazenda.gov.br/tesouro-direto-precos-e-taxas-dos-titulos/';
-		var body = '';
-		var selector = '#p_p_id_precosetaxas_WAR_tesourodiretoportlet_ > div > div > div > div.sanfonado > table > tbody';
-		
-		http.get(base_url, function(response) {
-			response.on('data', function(d) {
-				body += d;
-			});
+		process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; // The API uses a cipher not supported by recent versions (^14) of nodejs
+		http.get(api_url, (response) => {
+			response.on('data', d => body += d);
+			response.on('end', () => {
+				const td_response = JSON.parse(body);
 
-			response.on('end', function() {
-				var doc_cheerio = cheerio.load(body);
-				
-				var response_filtered = [];
-				var price_string;
-				var symbol_string;
-				var name_string;
-				var price_float;
+				let tdPrices = td_response.response.TrsrBdTradgList
+					.map((element) => {
+						let tdRaw = element.TrsrBd;
 
-				doc_cheerio(selector).children('.camposTesouroDireto').each(
-					function(i, element) {
-						symbol_string = element.children[1].children[0].data;
-
-						price_string = element.children[7].children[0].data;
-
-						// Remove 'R$' from price
-						price_string = price_string.replace('R$', '');
-
-						// // Remove point if it exists
-						price_string = price_string.replace('.', '');
-
-						// // Replace comma with point
-						price_string = price_string.replace(',', '.');
-
-						price_float = parseFloat(price_string);
-
-						name_string = mapSymbolIn2Out(symbol_string, td_list);
-
-						// If not found, don't need to push this object
-						if(name_string != -1) {
-							response_filtered.push({
-								'name'				: name_string,
-								'lastTradePriceOnly': price_float
-								// 'lastTradeDate'		: '',
-							});
+						return {
+							name: mapSymbolIn2Out(tdRaw.nm, td_list),
+							lastTradePriceOnly: tdRaw.untrInvstmtVal
 						}
-					}
-				);
+					})
+					.filter((element) => element.name !== -1 && element.lastTradePriceOnly > 0);
 
-				callback(null, response_filtered);
+				callback(null, tdPrices);
 			});
 		});
 	}
@@ -100,13 +68,13 @@ async.parallel([
 
 	// Parallel execution merged here	
 	function(err, raw_data) {
-		console.log(raw_data);
 		if(err) {
 			console.log(err);
 			return;
 		}
 		else {
 			var prices_combined = raw_data[0].concat(raw_data[1]);
+			console.log(prices_combined);
 
 			async.waterfall([
 				function(callback) {
@@ -161,15 +129,6 @@ function createCSV(data, callback) {
 		else
 			callback(null, csv);
 	});
-};
-
-function timestampToDateString(timestamp) {
-	var date = new Date(timestamp);
-
-	var date_string = '';
-	date_string = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-
-	return date_string;
 };
 
 function writeToFile(data, path, file_name, callback) {
